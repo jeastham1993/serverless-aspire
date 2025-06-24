002fd4a7937d9060b449361db6f256032f5082a6
james.eastham <james.eastham@datadoghq.com>
2025-06-24 14:54:27 +0100

    chore: disable warnings

--- AWS/Serverless.Aspire.AWS/TestMessages.cs
+++ AWS/Serverless.Aspire.AWS/TestMessages.cs
@@ -2,6 +2,8 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2025 Datadog, Inc.
 
+#pragma warning disable CA1812
+
 namespace Serverless.Aspire.AWS;
 
 internal sealed record ProductRestockedTestMessage(string ProductId, int NewStockLevel);
